4b27159fab3fea16b50ce4ad9bcbc3fa36951c7b
Delvin <70066996+delvin220@users.noreply.github.com>
2024-09-16 11:17:54 -0400

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by delvin han on 2024-06-21.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+    
+}
